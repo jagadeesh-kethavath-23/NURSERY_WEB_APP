12f3e8b1cf0fc2885b09ceae8fbad20b56d8396e
kethavath jagadeesh <kethavathjagadeesh4@gmail.com>
2021-05-03 20:00:04 +0530

    added 9 html pages and routed backend founctions for them

--- app.py
+++ app.py
@@ -16,16 +16,19 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+
 #----------------default page when the server gets loaded-----------------
 @app.route('/')
 def index():
     msg=''
     return render_template('index.html')
 
+
 #---------------for the error page-----------
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
 
 #--------------signin----------------------
 @app.route('/signin',methods=['GET','POST'])
@@ -43,7 +46,7 @@ def signin():
             session['user_id']=touple['user_id']
             msg = 'successfully logged in'
             if password == 'P@55word#admin#':
-            return render_template('adminhome.html',msg=msg)
+                return render_template('adminhome.html',msg=msg)
            
             if password == 'P@55word#manager#':
                 return render_template('managerhome.html',msg=msg)
@@ -52,6 +55,7 @@ def signin():
         return render_template('signin.html',msg=msg)
     msg = 'please fill out form'
     return render_template('signin.html',msg=msg)
+
 
 #-----------------signup------------------
 @app.route('/signup',methods=['GET','POST'])
@@ -66,75 +70,97 @@ def signup():
         password = request.form['password']
         address = request.form['address']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('select * for users where user_id = (%s)',(user_id))
+        cursor.execute('select * from users where user_id = (%s)',(user_id,))
         exists = cursor.fetchone()
         if exists:
             msg = 'this phone number is already in use'
             return render_template('signup.html',msg=msg)
         cursor.execute('insert into users values (%s,%s,%s,%s,%s)',(name,user_id,gender,password,address,))
         mysql.connection.commit()
+        session['user_id']='user_id'
         msg = 'successfully registered'
         if password == 'P@55word#admin#':
             return render_template('adminhome.html',msg=msg)
            
         if password == 'P@55word#manager#':
-             return render_template('managerhome.html',msg=msg)
+            return render_template('managerhome.html',msg=msg)
         return render_template('userhome.html',msg=msg)
         
     msg = 'please fill out form'
     return render_template('signup.html',msg=msg)
 
+
 #---------------shop by search---------------
-@app.route('/shopbysearch/<product_type>',methods['GET']) 
-def shopbysearch(product_type):
+@app.route('/shopbysearch',methods=['POST']) 
+def shopbysearch():
     msg = ''
-    if product_type == 'plants' or product_type =='seeds' or product_type == 'fertilizers' or product_type == 'accessories':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('select * from products where product_type = (%s)',(product_type,))
-        products = cursor.fetchall()
-        if products:
-            return render_template('shop.html',products=products)
-        msg=str(product_type)+ ' are out of stock'
-        render_template('shop.html',msg = msg)
-    msg = "no such services avaliable"
+    if 'product_type' in request.form:
+        product_type = request.form['product_type']
+        if product_type == 'plants' or product_type =='seeds' or product_type == 'fertilizers' or product_type == 'accessories':
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('select * from products where product_type = (%s)',(product_type,))
+            products = cursor.fetchall()
+            if products:
+                return render_template('shop.html',products=products)
+            msg=str(product_type)+ ' are out of stock'
+            render_template('shop.html',msg = msg)
+        msg = "no such services avaliable"
+        return render_template('shop.html',msg=msg)
+    msg = "please fill details"
     return render_template('shop.html',msg=msg)
+
 
 #------------shop---------------------
 @app.route('/shop',methods=['GET'])
 def shop():
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('select * from products')
-    products = cursor.fetchall()
-    if products:
-        return render_template('shop.html',products=products)
-    msg='All types of products are out of stock'
-    render_template('shop.html',msg = msg) 
+    msg=''
+    if 'user_id' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from products')
+        products = cursor.fetchall()
+        if products:
+            return render_template('shop.html',products=products,msg=msg)
+        msg='All types of products are out of stock'
+        render_template('shop.html',products=products,msg = msg) 
+    msg='login '
+    return render_template('shop.html',products=products,msg=msg)
+
 
 #-------------order and payment-----------------
-@app.route('/orders/<product_id>'methods=['GET','POST'])
+@app.route('/orders/<product_id>',methods=['GET','POST'])
 def orders(product_id):
     msg=''
     if 'user_id' in session:
-        cursor = mysql.connection.cursor(MySQLdb.cursors.Dictcursor)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('select * from products where product_id = (%s)',(product_id,))
         product = cursor.fetchone()
+        p= int( str(product['product_quantity']) )
         cursor.execute('select sum(order_quantity) as quantity from orders where product_id=(%s)',(product_id,))
         already_ordered = cursor.fetchone()
-        avaliable_product_quantity = int(product['quantity'])-int(already_ordered['quantity'])
+        q=0
+        if already_ordered['quantity']=='null':
+            q=0
+        
+            #q=int(  str(already_ordered['quantity'])   )
+        avaliable_product_quantity = p-q
         if request.method == 'GET':
             return render_template('discription.html',product=product,avaliable_product_quantity=avaliable_product_quantity)
-        if 'cvv' in request.form and 'card_number' in request.form and 'amount' in request.form and 'quantity' in request.form:
+        if 'cvv' in request.form and 'card_number' in request.form and 'quantity' in request.form:
+            quantity = request.form['quantity']
+            cvv = request.form['cvv']
+            card_number = request.form['card_number']
             ordered_date=date.today()
-            cursor.execute(select * from payment where card_number=(%s) and cvv=(%s) and user_id=(%s),(card_number,cvv,session['user_id'],))
+            cursor.execute('select * from payment where card_number=(%s) and cvv=(%s) and user_id=(%s)',(card_number,cvv,session['user_id'],))
             credentials=cursor.fetchone()
             if credentials:
                 m = (  int(quantity)  )*( int(product['cost']) )
-                if (int(credentials['balance']))>=m
-                    if quantity<=avaliable_product_quantity:
+                if (int(credentials['balance']))>=m:
+                    if int(quantity)<=avaliable_product_quantity:
                         cursor.execute('insert into orders values (%s,%s,%s,%s)',(session['user_id'],product_id,quantity,ordered_date,))
                         mysql.connection.commit()
                         balance = int(credentials['balance'])-m
                         cursor.execute('update payment set balance=(%s) where user_id=(%s) and card_number=(%s)',(balance,session['user_id'],card_number,))
+                        mysql.connection.commit()
                         avaliable_product_quantity = avaliable_product_quantity - int(quantity)
                         msg = 'successfully ordered'
                         return render_template('discription.html',product=product,avaliable_product_quantity=avaliable_product_quantity,msg=msg)
@@ -148,6 +174,8 @@ def orders(product_id):
         return render_template('discription.html',product=product,avaliable_product_quantity=avaliable_product_quantity,msg=msg)
     msg = 'please signin before ordering'
     return render_template('signin.html',msg=msg)
+
+
 #------------myorders---------------
 @app.route('/myorders')
 def myorders():
@@ -158,15 +186,58 @@ def myorders():
         my_orders = cursor.fetchall()
         if my_orders:
             msg='your orders'
-            return render_template('myorders.html',msg=msg)
+            return render_template('myorders.html',my_orders=my_orders,msg=msg)
         msg='no orders yet'
         return render_template('myorders.html',msg=msg)
     msg = 'please signin to check orders'
     return render_template('signin.html',msg = msg)
+
+
+#----------------checking reviews-------------------
+@app.route('/checkreview/<product_id>',methods=['GET'])
+def checkreview(product_id):
+    msg = ''
+    if 'user_id' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from products where product_id=(%s)',(product_id,))
+        product = cursor.fetchone()
+        cursor.execute('select * from reviews where product_id=(%s)',(product_id,))
+        reviews = cursor.fetchall()
+        if reviews:
+            msg='these are the reviews given by the users on this product'
+            return render_template('checkreview.html',product=product,reviews = reviews,msg= msg)
+        msg = 'no reviews yet on this product'
+        return render_template('checkreview.html',product=product,reviews = reviews,msg= msg)
+    msg='signin to check reviews'
+    return render_template('signin.html',msg=msg)
+
+
+#------------give review------------
+@app.route('/givereview/<product_id>',methods = ['GET','POST'])
+def givereview(product_id):
+    msg = ''
+    if 'user_id' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from products where product_id = (%s)',(product_id,))
+        product = cursor.fetchone()
+        if request.method=='GET':
+            return render_template('givereview.html',product = product,msg=msg)
+        if 'discription' in request.form and 'rating' in request.form :
+            discription = request.form['discription']
+            rating = request.form['rating']
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('insert into reviews values(%s,%s,%s,%s)',(product_id,session['user_id'],rating,discription,))
+            mysql.connection.commit()
+            msg = 'successfully reviewed'
+            return render_template('givereview.html',product = product,msg=msg)
+        msg = 'fil out form'
+        return render_template('givereview.html',product = product,msg=msg)
+    msg = 'signin to give review'
+    return render_template('signin.html',msg=msg)
 #------------add_to_cart------------
 #------------my_cart----------------
 #-----------remove_from_cart--------
-#----------reviews
+
     
 
     
